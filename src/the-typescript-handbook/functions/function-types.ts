b70861063265e244e163464c2d3ea99a25dc6234
Marty Zhang <marty8zhang@gmail.com>
2020-10-09 15:52:14 +1100

    Update code example(s) for "Functions" - "Function Types".

--- src/the-typescript-handbook/functions/function-types.ts
+++ src/the-typescript-handbook/functions/function-types.ts
@@ -26,6 +26,21 @@ function illegalAdd(x: number, y: number) {
 // myAdd = illegalAdd;
 
 /*
+ * Even though the return type isn't declared, TypeScript will still automatically determine
+ * and enforce the return type.
+ */
+function implicitlyReturnString(message: string) {
+  return message;
+}
+function acceptNumber(number: number) {
+  console.log(number);
+}
+/*
+ * TSError: Argument of type 'string' is not assignable to parameter of type 'number'.
+ */
+// acceptNumber(implicitlyReturnString('test'));
+
+/*
  * There is an exception for the `void` return type that TypeScript won't enforce the actual
  * return type to match with the declared `void` return type.
  * @see https://stackoverflow.com/questions/12761607/typescript-void-return-type-converted-to-any-type
